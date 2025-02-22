a8d7e545d4fb165f42d8c8684acf7cea3f3ed8f7
Kento555 <shen_kento555@hotmail.com>
2025-02-22 10:26:03 +0800

    added s3 block public policy

--- s3.tf
+++ s3.tf
@@ -2,6 +2,14 @@ resource "aws_s3_bucket" "static_site_bucket" {
   bucket = var.s3_domain_name
 }
 
+resource "aws_s3_bucket_public_access_block" "s3_block" {
+  bucket                  = aws_s3_bucket.static_site_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_ownership_controls" "s3_ownership" {
   bucket = aws_s3_bucket.static_site_bucket.id
   rule {
